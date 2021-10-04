1689d3679deba7a1d665fbff8267f2724ee70a2b
Tom <75093597+Skyrub-dev@users.noreply.github.com>
2021-10-04 09:29:06 +0100

    Update bot.py

--- Source/bot.py
+++ Source/bot.py
@@ -86,7 +86,6 @@ async def on_guild_join(guild):
 @bot.command(name = "ping")
 async def ping(ctx: commands.Context):
     await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
-    #await ctx.send = discord.Embed(title = "Pong!", description = {round(bot.latency * 1000)})
 
 @bot.command(name = "invite")
 async def invite(ctx):
